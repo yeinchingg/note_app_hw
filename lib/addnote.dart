786cb6e8d8f94d31e60e1b63f710aca625d244a8
weiting <tina.hsu0816@gmail.com>
2025-05-30 21:43:17 +0800

    update addnote

--- lib/addnote.dart
+++ lib/addnote.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
-class AddNotePage extends StatelessWidget {
+import 'model/note.dart';
+
+class AddNotePage extends StatefulWidget {
   const AddNotePage({super.key});
+
+  @override
+  State<AddNotePage> createState() => _AddNotePageState();
+}
+
+class _AddNotePageState extends State<AddNotePage> {
+  String title = "";
+  String description = "";
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Center(
-          child: TextButton(child: Text('confirm'),onPressed: () {Navigator.pop(context);}),
-        )
+      child: Center(
+        child: TextButton(
+          child: Text('confirm'),
+          onPressed: () {
+            Navigator.pop(
+              context,
+              Note(
+                title: title,
+                description: description,
+                dateTime: DateTime.now(),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
